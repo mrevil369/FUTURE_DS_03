85982a1eaa2593d3be8b9cb896e25b751abd973c
NATARAJ S <natarajshanmugam2005@gmail.com>
2025-08-07 11:36:14 +0530

    Update College Event Feedback Analysis.py

--- College Event Feedback Analysis.py
+++ College Event Feedback Analysis.py
@@ -1,19 +1,11 @@
 !pip install textblob wordcloud seaborn
-
 import pandas as pd
 from textblob import TextBlob
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud
 from google.colab import files
-
-# Upload the CSV file
 uploaded = files.upload()
-
-# Read it
 import io
 df = pd.read_csv(io.BytesIO(uploaded[list(uploaded.keys())[0]]), encoding='cp1252')
-
-
-# Display first few rows
 df.head()
